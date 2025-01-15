3b15ff0f008e7692015ed1e37b1c9d052e318678
francis-codex <francislogo2219@gmail.com>
2025-01-15 23:30:58 +0100

    finished and updated spl_metadata/spl_transfer

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,5 +1,5 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import wallet from "../turbin3-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,18 +10,42 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("BkzDWpverYNSRXsobMEqjNrsCRtThQaHbWZxNZ5zvjyX");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("9rxVXJ12mBoRrtegEjjs7KBkHuYaDJ5YjrbY4ZEuyQPy");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
+        const ata = await getOrCreateAssociatedTokenAccount(
+            connection, 
+            keypair, 
+            mint,
+            keypair.publicKey
+        );
 
-        // Transfer the new token to the "toTokenAccount" we just created
+         // Get the token account of the toWallet address, and if it does not exist, create it
+
+        const ata1 = await getOrCreateAssociatedTokenAccount(
+            connection, 
+            keypair, 
+            mint,
+            to
+        );
+
+         // Transfer the new token to the "toTokenAccount" we just created
+
+        const tx = await transfer(
+            connection, 
+            keypair,
+            ata.address,
+            ata1.address,
+            keypair,
+            1e6,
+         )
+        console.log(`Transaction signature: ${tx}`);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
